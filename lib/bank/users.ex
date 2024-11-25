15e6b902be57195bca9fd819d78f1d0701363a4a
r. ávila <avila.dev@outlook.com>
2024-11-24 22:34:10 -0300

    feat: implement user service

---
+++ lib/bank/users.ex
@@ -0,0 +1,66 @@
+defmodule Bank.Users do
+  alias Bank.Repo
+  alias Bank.Users.User
+
+  def list() do
+    with users when users != [] <- Repo.all(User) do
+      {:ok, users}
+    else
+      [] -> {:error, "no users found"}
+      _ -> {:error, "an unexpected error occurred"}
+    end
+  end
+
+  def insert(params) do
+    case params |> User.changeset() do
+      %Ecto.Changeset{valid?: true} = valid -> valid |> Repo.insert()
+      invalid -> {:error, invalid}
+    end
+  end
+
+  def update(params) do
+    case get(params) do
+      {:ok, user} ->
+        user
+        |> User.changeset(params)
+        |> Repo.update()
+
+      {:error, _reason} = error ->
+        error
+    end
+  end
+
+  def delete(%{"id" => id}) do
+    with {:ok, user} <- get(id),
+         {:ok, _} <- Repo.delete(user) do
+      {:ok}
+    else
+      {:error, _reason} = error -> error
+    end
+  end
+
+  def get(%{"id" => id}) when is_integer(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, "user not found"}
+      user -> {:ok, user}
+    end
+  end
+
+  def get(%{"email" => email}) when is_binary(email) do
+    case Repo.get_by(User, email: email) do
+      nil -> {:error, "user not found"}
+      user -> {:ok, user}
+    end
+  end
+
+  def get(id) when is_integer(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, "user not found"}
+      user -> {:ok, user}
+    end
+  end
+
+  def get(%{"id" => _id}), do: {:error, "id must be an integer"}
+  def get(%{"email" => _email}), do: {:error, "missing or malformed email"}
+  def get(_id), do: {:error, "missing or invalid id"}
+end
