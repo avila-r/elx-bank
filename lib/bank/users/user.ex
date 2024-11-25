1e1be404cdac34e02e70fe3884bcce034763db8c
r. ávila <avila.dev@outlook.com>
2024-11-25 12:14:54 -0300

    fix: include virtual fields in update changeset

--- lib/bank/users/user.ex
+++ lib/bank/users/user.ex
@@ -2,10 +2,13 @@ defmodule Bank.Users.User do
   import Ecto.Changeset
   use Ecto.Schema
 
+  def fields, do: __MODULE__.__schema__(:fields) ++ __MODULE__.__schema__(:virtual_fields)
+
   @required_params [:name, :email, :password]
   schema "users" do
     field :name, :string
     field :email, :string
+    field :cep, :string
 
     field :password, :string, virtual: true
     field :password_hash, :string
@@ -14,29 +17,30 @@ defmodule Bank.Users.User do
   end
 
   def changeset(params) do
-    fields = @required_params
+    fields = fields()
 
     %Bank.Users.User{}
     |> cast(params, fields)
-    |> validate(fields)
+    |> validate(@required_params)
     |> hash_password()
   end
 
   def changeset(struct, params) do
-    fields = @required_params
+    fields = fields() -- [:password_hash]
 
     struct
     |> cast(params, fields)
-    |> validate(fields -- [:password])
+    |> validate(@required_params -- [:password])
     |> hash_password()
   end
 
   defp validate(changeset, fields) do
     changeset
-    |> validate_required(fields)
+    |> validate_required(fields -- [:password])
     |> unique_constraint(:email)
     |> validate_length(:name, min: 3)
     |> validate_length(:password, min: 8)
+    |> validate_length(:cep, is: 9)
   end
 
   @spec to_json(map()) :: map()
