fc13ecc1a45ad2d445cc0f38bc5f111f5f279d72
r. ávila <avila.dev@outlook.com>
2024-11-24 22:39:13 -0300

    feat: create users controller

--- lib/bank_web/controllers/users_controller.ex
+++ lib/bank_web/controllers/users_controller.ex
@@ -1,21 +1,74 @@
 defmodule BankWeb.UsersController do
+  alias Bank.{Users, Users.User}
   use BankWeb, :controller
-  alias Bank.Users
 
-  def create(conn, new) do
-    new
-    |> Users.create()
-    |> case do
-      {:ok, user} ->
+  action_fallback BankWeb.FallbackController
+
+  def index(conn, _params) do
+    case Users.list() do
+      {:ok, users} ->
         conn
-        |> put_status(:created)
-        |> render(:create, user: user)
+        |> json(users |> Enum.map(&Users.User.to_json/1))
 
       {:error, reason} ->
         conn
-        |> put_status(:bad_request)
-        |> put_view(json: BankWeb.ErrorJSON)
-        |> render(:error, reason)
+        |> error(:not_found, message: reason)
     end
+  end
+
+  def show(conn, params) do
+    case params |> Users.get() do
+      {:ok, user} ->
+        conn
+        |> json(user |> User.to_json(:sensitive))
+
+      {:error, reason} ->
+        conn
+        |> error(:not_found, message: reason)
+    end
+  end
+
+  def create(conn, params) do
+    case params |> Users.insert() do
+      {:ok, user} ->
+        conn
+        |> put_status(:created)
+        |> json(user |> User.to_json(:sensitive))
+
+      {:error, changeset} ->
+        conn
+        |> error(:unprocessable_entity, changeset: changeset)
+    end
+  end
+
+  def update(conn, params) do
+    case params |> Users.update() do
+      {:ok, user} ->
+        conn
+        |> json(user |> User.to_json(:sensitive))
+
+      {:error, changeset} ->
+        conn
+        |> error(:unprocessable_entity, changeset: changeset)
+    end
+  end
+
+  def delete(conn, params) do
+    case params |> Users.delete() do
+      {:ok} ->
+        conn
+        |> send_resp(:no_content, "")
+
+      {:error, reason} ->
+        conn
+        |> error(:bad_request, message: reason)
+    end
+  end
+
+  defp error(conn, status, error) do
+    conn
+    |> put_status(status)
+    |> put_view(BankWeb.ErrorJSON)
+    |> render(:error, error)
   end
 end
