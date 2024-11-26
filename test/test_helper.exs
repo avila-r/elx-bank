127c667e7184d2f89d2f1ef1ee3345ed10ed1e8f
r. ávila <avila.dev@outlook.com>
2024-11-25 22:37:56 -0300

    test(build): set up mocks for viacep client

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,7 @@
+alias Bank.Viacep.ClientBehaviour
+
+Mox.defmock(Bank.Viacep.ClientMock, for: ClientBehaviour)
+Application.put_env(:bank, :viacep_client, Bank.Viacep.ClientMock)
+
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Bank.Repo, :manual)
