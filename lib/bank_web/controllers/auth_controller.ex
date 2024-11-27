41492d5bddba51cd55866a508d456fd4084fd114
r. ávila <avila.dev@outlook.com>
2024-11-26 22:48:54 -0300

    feat: implement auth controller

---
+++ lib/bank_web/controllers/auth_controller.ex
@@ -0,0 +1,26 @@
+defmodule BankWeb.AuthController do
+  use BankWeb, :controller
+
+  action_fallback BankWeb.FallbackController
+
+  alias Bank.{Users, Users.User, Token}
+
+  def login(conn, params) do
+    case Users.login(params) do
+      {:ok, user} ->
+        conn
+        |> json(%{
+          token: user |> Token.new(),
+          user: user |> User.to_json()
+        })
+
+      {:error, :unauthorized} ->
+        conn
+        |> Errors.send(:unauthorized)
+
+      {:error, reason} ->
+        conn
+        |> Errors.send(:not_found, message: reason)
+    end
+  end
+end
